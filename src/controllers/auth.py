80fce07665f093650c92989468e5d48d0a8a79c5
Fabiana Masini <masinifabiana@gmail.com>
2026-06-15 17:22:15 -0300

    Recuperacao de senha

--- src/controllers/auth.py
+++ src/controllers/auth.py
@@ -1,6 +1,8 @@
-from flask import render_template, redirect, url_for, flash
-from flask_login import login_user, logout_user
+from models import db
 from .user import UserController
+from .utils import is_password_strong
+from flask_login import login_user, logout_user
+from flask import render_template, redirect, url_for, flash
 
 class AuthController:
     def login(self, req, current_user=None):
@@ -35,6 +37,37 @@ class AuthController:
             return redirect(url_for('home'))
 
         return render_template('login.html')
+
+
+    def forgot_password(self, request):
+        if request.method == 'POST':
+            username = request.form.get('username')
+            username = username.strip() if username else ''
+            new_password = request.form.get('new_password')
+            confirm_new_password = request.form.get('confirm_new_password')
+
+            if not username:
+                flash('O nome de usuário é obrigatório.', 'error')
+                return redirect(url_for('forgot_password'))
+
+            user = UserController().get_user_by_username(username)
+
+            if user is None:
+                flash('Usuário não encontrado.', 'error')
+                return redirect(url_for('forgot_password'))
+            elif new_password != confirm_new_password:
+                flash('As senhas devem ser iguais.', 'error')
+                return redirect(url_for('forgot_password'))
+            elif not is_password_strong(new_password):
+                flash('A senha deve ter no mínimo 8 caracteres, contendo pelo menos 1 número e 1 caractere especial.', 'error')
+                return redirect(url_for('forgot_password'))
+            else:
+                user.set_password(new_password)
+                db.session.commit()
+                flash('Sua senha foi redefinida com sucesso. Faça login com a nova senha.', 'success')
+                return redirect(url_for('login'))
+        
+        return render_template('forgot_password.html')
     
     def logout(self):
         logout_user()
--- src/controllers/signup.py
+++ src/controllers/signup.py
@@ -1,17 +1,9 @@
-import re
 from .user import UserController
+from .utils import is_password_strong
 from werkzeug.security import generate_password_hash
 from flask import flash, redirect, url_for, render_template
 
 class SignupController:
-    def __is_password_strong(self, password):
-        if len(password) < 8:
-            return False
-        if not re.search(r"\d", password): # Pelo menos 1 número
-            return False
-        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password): # Pelo menos 1 especial
-            return False
-        return True
     
     def signup(self, request, current_user=None):
         if current_user.is_authenticated:
@@ -29,7 +21,7 @@ class SignupController:
             elif password != confirm_password:
                 flash('As senhas devem ser iguais.', 'error')
                 return redirect(url_for('signup'))
-            elif not self.__is_password_strong(password):
+            elif not is_password_strong(password):
                 flash('A senha deve ter no mínimo 8 caracteres, contendo pelo menos 1 número e 1 caractere especial.', 'error')
                 return redirect(url_for('signup'))
             else:
